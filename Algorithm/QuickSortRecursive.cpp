b93314ae8ef9d568106cf463098b5350b1dedd8c
jiangyibo <yibo.jiang@sharkninja.com>
2021-08-23 14:07:42 +0800

    add: 1.bucket sort; 2.count sort; 3.merge sort; 4.quick sort; 5.radix sort; 6.shell sort

--- Algorithm/QuickSortRecursive.cpp
+++ Algorithm/QuickSortRecursive.cpp
@@ -2,28 +2,32 @@
 #include <vector>
 #include <random>
 
-/*
-    （小数，基准元素，大数）。在区间中随机挑选一个元素作基准，将小于基准的元素放在基准之前，大于基准的元素放在基准之后，再分别对小数区与大数区进行排序。
-    快速排序思路：
-    1. 选取第一个数为基准
-    2. 将比基准小的数交换到前面，比基准大的数交换到后面
-    3. 对左右区间重复第二步，直到各区间只有一个数
-*/
-
 template<typename T>
-void quickSortRecursive(std::vector <T> &vec, int start, int end)
+void quickSortRecursive(std::vector<T> &vec, size_t start, size_t end)
 {
-    if (end <= start)
+    if (start >= end)
         return;
 
-    T key = vec[end];
-    int left = start;
+    if (start > vec.size() || end > vec.size())
+        return;
+
+    if (vec.size() < 2)
+        return;
+    
+    else if (vec.size() == 2)
+    {
+        if (vec[0] > vec[1])
+            std::swap(vec[0], vec[1]);
+    }
+
+    T key     = vec[end];
+    int left  = start;
     int right = end - 1;
 
     while (right > left)
     {
-        while (right > left && vec[left] <= key) ++left;
-        while (right > left && vec[right] >= key) --right;
+        while (left < right && vec[left]  <= key) ++left;
+        while (left < right && vec[right] >= key) --right;
 
         std::swap(vec[left], vec[right]);
     }
@@ -33,19 +37,20 @@ void quickSortRecursive(std::vector <T> &vec, int start, int end)
     else
         ++left;
 
-    quickSortRecursive(vec, start, left - 1);
+    quickSortRecursive(vec, start,    left - 1);
     quickSortRecursive(vec, left + 1, end);
 }
 
 int main()
 {
-    std::vector<float> vec{1.1, 3.2, 1.2, 23, 3.2, 12.3, 3.2, 5.4, 7, 3.2, 3.2, 3.2};
+    std::vector<int> vec;
+    vec.resize(10);
 
-    std::default_random_engine e;
-    std::uniform_real_distribution<float> u(0, 100);
+    std::random_device rd;
+
     for (size_t i = 0; i < vec.size(); i++)
     {
-        vec[i] = u(e);
+        vec[i] = rd() % 50;
     }
 
     std::cout << "old: ";
@@ -55,8 +60,7 @@ int main()
     }
     std::cout << std::endl;
 
-    int len = vec.size() - 1;
-    quickSortRecursive(vec, 0, len);
+    quickSortRecursive(vec, 0, vec.size() - 1);
 
     std::cout << "new: ";
     for (auto tmp: vec)
@@ -64,6 +68,5 @@ int main()
         std::cout << tmp << "     ";
     }
     std::cout << std::endl;
-
     return 0;
 }
